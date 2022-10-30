985a1dea4508c7a4783b87968bfd2111a790eda3
EC2 Default User <ec2-user@ip-172-31-7-46.sa-east-1.compute.internal>
2022-10-30 19:27:36 +0000

    test

--- preinstall.sh
+++ preinstall.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
-cd /etc/nginx/conf.d/
+cd ~
 sudo git clone https://github.com/LoopHunter/nginx.git
+cd ~/nginx
+sudo mv backend.conf /etc/nginx/conf.d/
 sudo systemctl restart nginx
+sudo chown -R ec2-user:ec2-user ~/test_asg_project
 npm install pm2 -g
