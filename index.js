65d9977c6117aa464b0ef6c1a3344efed64798d6
MUKUL JARIYAL <84459415+LoopHunter@users.noreply.github.com>
2022-10-30 12:37:26 +0530

    inital

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+require('dotenv').config()
+const port = process.env.PORT || 3000;
+const output = process.env.OUTPUT || 'Please provide output in .env';
+const app = express();
+
+// main
+app.get('/', (req, res) => {
+    return res.status(200).json({ status: 'success', data: `${output} is working fine V2` });
+});
+
+// health check
+app.get('/health_check', (req, res) => {
+    return res.status(200).json({ status: 'success' });
+});
+
+var http = require('http').createServer(app);
+http.listen(port, () => console.log(`App listening on port: ${port}`));
